6a41903406e85a4bcb848d4cf713f79b72a5d26e
Yash Jagtap <jagtapyash2512@gmail.com>
2023-03-24 10:52:58 +0530

    import address and abi

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import "./App.css";
 
+import { ethers } from "ethers";
+import Counter from "./contracts/Counter.sol/Counter.json";
+const counterAddress = "0x5200aA2A72a96A3091831e812edC5De4ad6DB426"
+console.log(counterAddress, "Counter ABI: ", Counter.abi);
+
 function App() {
   const [count, setCount] = useState(0);
 
